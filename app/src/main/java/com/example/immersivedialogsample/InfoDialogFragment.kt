7f1e59afc5b7d13d3c7b29e6a2ebd5241f0a0e61
tktcorporation <tktcorporation.go@gmail.com>
2021-06-11 03:39:46 +0900

    :recycle: ナビゲーションバーが一瞬出る

--- app/src/main/java/com/example/immersivedialogsample/InfoDialogFragment.kt
+++ app/src/main/java/com/example/immersivedialogsample/InfoDialogFragment.kt
@@ -1,13 +1,10 @@
 package com.example.immersivedialogsample
 
-import android.app.Dialog
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
-import android.view.View
-import android.view.WindowManager
+import android.view.*
 import androidx.fragment.app.DialogFragment
-import androidx.fragment.app.FragmentTransaction
 import com.example.immersivedialogsample.databinding.DialogInfoBinding
 
 
@@ -16,38 +13,54 @@ import com.example.immersivedialogsample.databinding.DialogInfoBinding
 class InfoDialogFragment : DialogFragment() {
     lateinit var binding: DialogInfoBinding
 
-    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-        val dialog = Dialog(requireContext())
-        // ダイアログの背景を透過にする
-        dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
-
-        binding = DialogInfoBinding.inflate(requireActivity().layoutInflater)
-
-        binding.closeButton.setOnClickListener {
-            dismiss()
-        }
-
-        dialog.setContentView(binding.root)
-
-        dialog.window?.decorView?.systemUiVisibility = (View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                or View.SYSTEM_UI_FLAG_FULLSCREEN
-                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN)
-
-        return dialog
-    }
-
-    override fun show(transaction: FragmentTransaction, tag: String?): Int {
-        dialog?.window?.setFlags(
-            WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
-            WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = DialogInfoBinding.inflate(
+            inflater
         )
 
-        val result = super.show(transaction, tag)
+        //设置背景透明
+        dialog?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        //关闭点击外面消失的效果
+        dialog?.setCanceledOnTouchOutside(true)
+        //去掉低版本部分机型顶部出现一条蓝线的现象
+        dialog?.requestWindowFeature(Window.FEATURE_NO_TITLE)
 
-        dialog?.window?.clearFlags(WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE)
+        //window背后所以变灰暗
+        dialog?.window?.setFlags(
+            WindowManager.LayoutParams.FLAG_DIM_BEHIND,
+            WindowManager.LayoutParams.FLAG_DIM_BEHIND
+        )
+        dialog?.window?.setDimAmount(0.7f)
 
-        return result
+
+        //不要焦点，保持全屏
+        dialog?.window?.addFlags(WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE)
+         dialog?. window?.setFlags(WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE, WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE)
+        fullScreenImmersive(dialog?.window?.decorView!!)
+         dialog?. window?.clearFlags(WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE)
+        return binding.root
+    }
+
+    private fun fullScreenImmersive(view: View) {
+        val uiOptions = (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                or View.SYSTEM_UI_FLAG_FULLSCREEN)
+        view.systemUiVisibility = uiOptions
+        view.setOnSystemUiVisibilityChangeListener {
+            var uiOptions = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or  //布局位于状态栏下方
+                    View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or  //全屏
+                    View.SYSTEM_UI_FLAG_FULLSCREEN or  //隐藏导航栏
+                    View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or
+                    View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+            uiOptions = uiOptions or 0x00001000
+            view.systemUiVisibility = uiOptions
+        }
     }
 }
