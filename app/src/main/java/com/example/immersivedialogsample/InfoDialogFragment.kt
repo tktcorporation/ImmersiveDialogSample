2afa310c2ebba64d2f6fa4731330be4971c0ba6a
tktcorporation <tktcorporation.go@gmail.com>
2021-06-11 04:02:32 +0900

    :sparkles: 完璧

--- app/src/main/java/com/example/immersivedialogsample/InfoDialogFragment.kt
+++ app/src/main/java/com/example/immersivedialogsample/InfoDialogFragment.kt
@@ -30,7 +30,8 @@ class InfoDialogFragment : DialogFragment() {
         //设置背景透明
         dialog?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
         //关闭点击外面消失的效果
-        dialog?.setCanceledOnTouchOutside(true)
+        dialog?.setCanceledOnTouchOutside(false)
+        dialog?.setCancelable(true)
         //去掉低版本部分机型顶部出现一条蓝线的现象
         dialog?.requestWindowFeature(Window.FEATURE_NO_TITLE)
 
@@ -46,6 +47,11 @@ class InfoDialogFragment : DialogFragment() {
         dialog?.window?.addFlags(WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE)
          dialog?. window?.setFlags(WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE, WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE)
         fullScreenImmersive(dialog?.window?.decorView!!)
+
+        binding.closeButton.setOnClickListener {
+            dismiss()
+        }
+
         return binding.root
     }
 
@@ -68,7 +74,7 @@ class InfoDialogFragment : DialogFragment() {
     }
 
     private fun fullScreenImmersive(view: View) {
-        val uiOptions = (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+        var uiOptions = (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                 or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
                 or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
                 or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
@@ -76,11 +82,6 @@ class InfoDialogFragment : DialogFragment() {
                 or View.SYSTEM_UI_FLAG_FULLSCREEN)
         view.systemUiVisibility = uiOptions
         view.setOnSystemUiVisibilityChangeListener {
-            var uiOptions = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or  //布局位于状态栏下方
-                    View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or  //全屏
-                    View.SYSTEM_UI_FLAG_FULLSCREEN or  //隐藏导航栏
-                    View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or
-                    View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
             uiOptions = uiOptions or 0x00001000
             view.systemUiVisibility = uiOptions
         }
